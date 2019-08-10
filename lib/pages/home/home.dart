659663845665037f0abb672730372e03b1fcb1b0
Martin Twum Mensah <martint.mensah@gmail.com>
2019-08-10 14:39:13 +0000

    refactor: Add Scaffold Set bottomNavigationBar to BottomTabBar

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:Zygote_Nike_Layout/widgets/bottomNavBar.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,22 +15,25 @@ class _HomeState extends State<Home> {
     screenWidth = MediaQuery.of(context).size.width;
     screenHeight = MediaQuery.of(context).size.height;
 
-    return Container(
-      color: Theme.of(context).backgroundColor,
-      child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                // IconButton(
-                //   icon: Icon(Icons.arrow_back),
-                //   onPressed: () {},
-                // ) 
-              ],
-            )
-          ],
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).backgroundColor,
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  // IconButton(
+                  //   icon: Icon(Icons.arrow_back),
+                  //   onPressed: () {},
+                  // )
+                ],
+              )
+            ],
+          ),
         ),
       ),
+      bottomNavigationBar: BottomTabBar(),
     );
   }
 }
